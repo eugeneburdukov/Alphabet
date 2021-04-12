e000614a001a5beeafd6c07b897734071e407c86
Eugene Burdukov <74274237+eugeneburdukov@users.noreply.github.com>
2021-04-12 21:17:57 +0300

    commit

--- app/src/main/java/com/alphabet/alphabet/ResultsActivity.java
+++ app/src/main/java/com/alphabet/alphabet/ResultsActivity.java
@@ -8,17 +8,22 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.StyleSpan;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ResultsActivity extends AppCompatActivity {
+public class ResultsActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView imageViewStar;
     TextView textViewResultCor;
+    TextView textViewHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_layout);
+
+        textViewHome = findViewById(R.id.textViewHome);
+        textViewHome.setOnClickListener(this);
 
         imageViewStar = findViewById(R.id.imageViewStar);
         textViewResultCor = findViewById(R.id.textViewResultCor);
@@ -26,12 +31,21 @@ public class ResultsActivity extends AppCompatActivity {
 
         Intent mIntent = getIntent();
         int intValue1 = mIntent.getIntExtra("correctAnswer", 0);
-        int intValue2 = mIntent.getIntExtra("incorrectAnswer", 0);
 
-        String text = "Score " + intValue1 + " /  26";
-        SpannableString spannableString = new SpannableString(text);
+        String score = "Score " + intValue1 + " /  26";
+        SpannableString spannableString = new SpannableString(score);
         StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
         spannableString.setSpan(boldSpan, 6, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textViewResultCor.setText(spannableString);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textViewHome:
+                Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        }
     }
 }
